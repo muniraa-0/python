c8b362648cb08a2628047b1b45df160055eab23a
muniraa-0 <hosneara33@gmail.com>
2026-03-04 14:32:32 +0600

    3

---
+++ module 1/lesson 3/activity2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name")
+print("Hello, ", name + " Welcome to Codingal. ")
+str1 = "Munira"
+str2 = "Nigar"
+print(str1.upper())
+print(str1.lower())
+print(str1[::-1])
+print(str1+" "+str2)
