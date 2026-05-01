ac0ed47c91b866a1f7264c4d736d7671ad99573b
muniraa-0 <hosneara33@gmail.com>
2026-05-01 12:11:35 +0600

    hh

---
+++ module 3/lesson 5/acp.py
@@ -0,0 +1,17 @@
+import math
+
+
+angle = float(input("Enter angle in degrees: "))
+
+
+radian = math.radians(angle)
+
+
+sin_value = math.sin(radian)
+cos_value = math.cos(radian)
+tan_value = math.tan(radian)
+
+
+print("sin(", angle, ") =", sin_value)
+print("cos(", angle, ") =", cos_value)
+print("tan(", angle, ") =", tan_value)
