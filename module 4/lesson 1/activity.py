8e4c7bf52d1865a25fb1e2b9ee94fe48c6abea85
muniraa-0 <hosneara33@gmail.com>
2026-05-03 19:50:03 +0600

    jkkk

---
+++ module 4/lesson 1/activity.py
@@ -0,0 +1,9 @@
+list1 = ["physics","chemistry",1997,2000]
+print(len(list1))
+print(list1[2])
+list2 = list1[0:2]
+print(list2)
+for item in list1:
+    print(item)
+list3 = list1 + list2
+print(list3)
