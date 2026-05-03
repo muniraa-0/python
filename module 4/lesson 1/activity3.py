cbfda53a05df3681c21aa9193ae6e1ffb6f36107
muniraa-0 <hosneara33@gmail.com>
2026-05-03 20:12:42 +0600

    hhh

---
+++ module 4/lesson 1/activity3.py
@@ -0,0 +1,15 @@
+1 = [4,5,3,24,37,777,87,9]
+print("Orginal listL",1)
+
+sum = 0
+
+for num in 1:
+    sum = sum + num
+
+avg = sum/lem(1)
+print("Average:",avg)
+
+1,sort()
+
+print("smallest number:",1[0])
+print("lasrgest number:",1[-1])
