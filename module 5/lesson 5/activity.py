8786c2e453a28b3c95d418f7ca8dd933b42a9661
muniraa-0 <hosneara33@gmail.com>
2026-06-18 20:45:55 +0600

    hi

---
+++ module 5/lesson 5/activity.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+class absclass(ABC):
+    def print(self,x):
+        self.print("value: ",x)
+    @abstractmethod
+    def task(self):
+        self.print("This is an abstract method")
+class test_class(absclass):
+    def task(self):
+        self.print("This is a sub class")
+test_obj = test_class()
+test_class.task()
